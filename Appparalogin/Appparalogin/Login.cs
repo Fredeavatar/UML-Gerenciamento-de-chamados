f2f8d098b4d30825f7435c1b4074275221d95f4e
Frederico Dellu <fredericobarreto@hotmail.com.br>
2025-09-01 04:21:05 -0300

    Atualizado com versao desktop para login em windows forms

---
+++ Appparalogin/Appparalogin/Login.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace Appparalogin
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtUsuario.Text == "admin" && txtSenha.Text == "123")
+                {
+                    var menu = new MenuRestrito();
+
+                    menu.Show();
+
+
+                    this.Visible = false;
+                    //this.Dispose();
+                    
+
+                }
+                else
+                {
+                    MessageBox.Show("Usuário ou senha inválidos",
+                                    "Atenção!!",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Error);
+
+                    txtUsuario.Focus();
+                    txtSenha.Text = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocorreu um erro: " + ex.Message,
+                                "Atenção!!",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+
+            }
+        }
+    }
+}
