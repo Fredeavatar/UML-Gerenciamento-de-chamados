bcaa22a1b754c9b4e3edcdc3276b90ceb653a67e
Frederico Dellu <fredericobarreto@hotmail.com.br>
2025-09-01 07:05:37 -0300

    Update App para login

--- Appparalogin/Appparalogin/Login.cs
+++ Appparalogin/Appparalogin/Login.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.Sql;
-using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Appparalogin
 {
@@ -53,6 +54,45 @@ namespace Appparalogin
                                 MessageBoxButtons.OK,
                                 MessageBoxIcon.Error);
 
+            }
+
+        }
+
+        private void txtUsuario_Enter(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == "Digite seu usuário, apenas letras ou números.")
+            {
+                txtUsuario.Text = "";
+                txtUsuario.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtUsuario_Leave(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == "")
+            {
+                txtUsuario.Text = "Digite seu usuário, apenas letras ou números.";
+                txtUsuario.ForeColor = SystemColors.InactiveCaption;
+            }
+        }
+
+        private void txtSenha_Enter(object sender, EventArgs e)
+        {
+            if (txtSenha.Text == "Senha")
+            {
+                txtSenha.Text = "";
+                txtSenha.ForeColor = Color.Black;
+                txtSenha.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void txtSenha_Leave(object sender, EventArgs e)
+        {
+            if (txtSenha.Text == "")
+            {
+                txtSenha.Text = "Senha";
+                txtSenha.ForeColor = SystemColors.InactiveCaption;
+                txtSenha.UseSystemPasswordChar = false;
             }
         }
     }
